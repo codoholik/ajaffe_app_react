509125b671058a052b1dcf2db61e1b67ec3a34c5
rishulayerfive <61156797+rishulayerfive@users.noreply.github.com>
2023-12-04 22:30:09 +0530

    Change endpoint to vabackend

--- src/loadallorders.js
+++ src/loadallorders.js
@@ -11,7 +11,7 @@ export default function LoadAllOrders() {
     const [orders, setorders] = useState(null);
 
     useEffect(_ => {
-        axios.get(`https://va.ajaffe.com:8000/get_all_orders?order_num=${orderno}`)
+        axios.get(`https://vabackend.ajaffe.com/get_all_orders?order_num=${orderno}`)
         .then(resp => {
             const result = resp.data;
             setsalesData({
